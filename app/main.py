8346dd6d3501610844f0d3d2a6648e027ace3c9a
Petros Tepoyan <petros@tepoyan.ru>
2022-11-16 16:11:20 +0400

    fixes

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__, static_url_path='/static')
 
+@app.route("/")
+def home_view():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
+
 @app.route('/incomes')
 def get_incomes():
     return jsonify(incomes)
